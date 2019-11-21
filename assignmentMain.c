a7c8f67106824fa806526beb0b7e3301eafbf647
CiaraSookarry <cjsookarry@gmail.com>
2019-11-21 09:45:22 -0800

    Added comments and started on the user interface

--- assignmentMain.c
+++ assignmentMain.c
@@ -1,12 +1,17 @@
 //assignmentMain.c
 //Main file for CE4703 assignment 3
-//Author: Ciara Sookarry
-//Date: 21/11/19
+//Contains user interface for user inputs
+//Authors: Ciara Sookarry, 
+//Date: 21/11/19 (Update as necessary)
 
 #include <stdlib.h>
 #include <stdio.h>
 
 int main()
 {
-
+  int highestExp; //user-entered value: largest value of n for x^n
+  
+  printf("Let's enter a polynomial!!\n");
+  printf("Enter highest exponent: ");
+  scanf("%d", &highestExp);
 }
