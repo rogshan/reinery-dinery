95df5a6a49d77f9ba8d09d13a1703393212d3d94
annashox <annashaughnessy100@gmail.com>
2019-11-21 17:50:11 +0000

    <What I was working on in Kilmurry. Unsure of calling the functioms in the main. Also sorry for lack of comments.>

--- assignmentMain.c
+++ assignmentMain.c
@@ -6,7 +6,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main()
-{
+void fillArray(double[] array, int highest_exponent, double[] coefficient ){ 
+  int j;
+  double[] array;
+  for (j=0, j>=coefficient[highest_exponent], j++){
+    array[j]= coefficient[j]*(x^(j));
+  }
+}
 
+void printArray(int highest_exponent, double[] array){
+  int z;
+  for (z=highest_exponent, z>=0, --z){
+    printf("The polynomial is: %lf", array[z]);
+  }
+}
+
+
+int main(){
+  int highest_exponent;
+  printf("Input value for highest value exponent\n:");
+  scanf("%d", &highest_exponent);
+  int[] coefficient;
+  
+  int i;
+  for (i=0, i<=highest_exponent, i++){
+      printf("Input the coefficient for the highest value exponent:\n");
+      scanf("%lf",&coefficient[i]);
+	i=i+1;
+    }
+  fillArray();
+  printArray();
 }
