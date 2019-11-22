501e8e9d8ecd8a20218d43a4a82fff5ac9f2ef6f
CiaraSookarry <cjsookarry@gmail.com>
2019-11-22 03:43:41 -0800

    Deleted main(). Made the bones of addPoly(), subtractPoly() and multiplyPoly().But they have no actual code, just comments.

--- assignment.c
+++ assignment.c
@@ -1,10 +1,36 @@
-//this is a test file
+//assignment.c
 //
-//date: 2019/11/21
-//author: roger shanahan
+//Contains operations for use on polynomials
+//date: 2019/11/22
+//author: roger shanahan, Ciara Sookarry
 
-int main()
+//I think we'll need to make a polynomial type
+
+//double addPoly(double coeff1, coeff2)
+//Adds coefficients of each x^n term
+//Maybe uses a for loop?????
+//Inputs: coefficients of 2 terms
+//Output: sum of 2 coefficents
+double addPoly(double coeff1, double coeff2)
 {
-    printf("This is a test file\n");
-    return 0;
+  //I'm not really sure how to code this right now
+}
+
+//double subtractPoly(double coeff1, coeff2)
+//Subtractss coefficients of each x^n term
+//Maybe uses a for loop?????
+//Inputs: coefficients of 2 terms
+//Output: sum of 2 coefficents
+double subtractPoly(double coeff1, double coeff2)
+{
+  //I'm not really sure how to code this right now
+}
+
+//double multiplyPoly(polynomial poly, double constant)
+//Multiplies each coefficient by a double and returns each
+//Inputs: polynomial type, double constant
+//Output: polynomial with each coefficient multiplied by a double
+double multiplyPoly()
+{
+  
 }
