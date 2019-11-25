2db96e9053b7a4aec6bfce1d4d7cbb816a26c298
annashox <annashaughnessy100@gmail.com>
2019-11-25 18:18:53 +0000

    Fixed issue where variable 'highest_exponent' was referred to as 'high_exponent'

--- assignmentMain.c
+++ assignmentMain.c
@@ -9,18 +9,18 @@
 #include <math.h>
 #include "assignment.h"
     
-void fillArray(double *ptrArray[], int high_exponent, double coefficient[]) //Function to fill coefficient array with user inputted values
+void fillArray(double *ptrArray[], int highest_exponent, double coefficient[]) //Function to fill coefficient array with user inputted values
 { 
-  for (int j=high_exponent; j >= coefficient[high_exponent]; j--)//For loop to fill array with value of the exponent order of decreasing value 
+  for (int j=highest_exponent; j >= coefficient[highest_exponent]; j--)//For loop to fill array with value of the exponent order of decreasing value 
   {
     double value = (coefficient[j] );
     ptrArray[j] = &value;
   }
 }
 
-void printArray(double *ptrArray[], int high_exponent) //Function to print array.
+void printArray(double *ptrArray[], int highest_exponent) //Function to print array.
 {
-  for (int z=high_exponent; z>=0; --z) //For loop to print array with the highest exponent value first, decreasing down
+  for (int z=highest_exponent; z>=0; --z) //For loop to print array with the highest exponent value first, decreasing down
   {
     printf("The polynomial is: %lf x^ %d", *ptrArray[z], z);
   }
