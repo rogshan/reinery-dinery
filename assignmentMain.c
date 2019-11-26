cfa9e3e9f71e09fa2ffd0839cdc162e3d70d280c
annashox <annashaughnessy100@gmail.com>
2019-11-26 17:42:50 +0000

    In previous commit I had '#include assignment.h commentted out. This commit was just to include that again

--- assignmentMain.c
+++ assignmentMain.c
@@ -2,12 +2,12 @@
 //Main file for CE4703 assignment 3
 //Contains user interface for user inputs
 //Authors: Ciara Sookarry, Roger Shanahan, Aoife O`Doherty, Anna Shaughnessy
-//Date: 25/11/19 (Update as necessary)
+//Date: 26/11/19 (Update as necessary)
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-//#include "assignment.h"
+#include "assignment.h"
 
 
 struct node 
