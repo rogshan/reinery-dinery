63dfca58b4429b77b3b35dee073c3e2e386d2216
CiaraSookarry <cjsookarry@gmail.com>
2019-12-04 06:19:58 -0800

    Added a small comment

--- assignmentMain.c
+++ assignmentMain.c
@@ -2,13 +2,16 @@
 //Main file for CE4703 assignment 3
 //Contains user interface for user inputs
 //Authors: Ciara Sookarry, Roger Shanahan, Aoife O`Doherty, Anna Shaughnessy
-//Date: 26/11/19 (Update as necessary)
+//Date: 30/11/19 (Update as necessary)
 
 #include <stdlib.h>
 #include <stdio.h>
 #include "assignment.h"
 
-
+//////////////////////////////
+// main()
+// tests the functions in assignemnt.c
+//////////////////////////////
 int main()
 {
   //int highest_exponent = 0;
