805765894159d7fccf878357abf6f144b6f3c7d3
CiaraSookarry <cjsookarry@gmail.com>
2019-12-08 16:45:45 +0000

    Tried to create tests
    I'm not sure if these would even work but it's something. My laptop is broken so I can't do more and can't see if these would compile or not. Sorry

--- assignmentMain.c
+++ assignmentMain.c
@@ -14,10 +14,26 @@
 //////////////////////////////
 int main()
 {
-  //int highest_exponent = 0;
-  //int max_coefficients = highest_exponent+1;
-  //createNodeList(max_coefficients);
-  //displayList(highest_exponent);
+  // Tests the createPoly function
+  // Sees is return value is null or not
+  createPoly();
+  //if NULL returned
+  if(){
+    printf("createPoly() failed test");
+  }else{
+    printf("createPoly() passed test");
+  }
+  
+   // Tests the addNode() function
+  // Sees is return value is a pointer to a new node or not
+  addNode(newNode, poly);
+  //if pointer returned
+  if(){
+    printf("addNode() passed test");
+  }else{
+    printf("addNode() failed test");
+  }
+  
   return 0;
 }
 
